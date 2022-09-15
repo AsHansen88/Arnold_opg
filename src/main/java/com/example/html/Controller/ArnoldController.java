d5f1d57d46b1343e523b03f2adab9f3aa61abdd5
bobby <ande970s@stud.kea.dk>
2022-09-15 15:25:13 +0200

    Done

---
+++ src/main/java/com/example/html/Controller/ArnoldController.java
@@ -0,0 +1,22 @@
+package com.example.html.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ArnoldController {
+
+  @GetMapping("/")
+  public String index(){
+    return "index";
+
+    }
+
+    @GetMapping("/about")
+    public String about(){
+    return "about";
+    }
+
+
+}
